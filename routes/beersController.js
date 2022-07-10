39be71b5f9979c6de7539bad4e36a2ef6dc4a89e
Yoann <yoann.v59@outlook.fr>
2022-07-10 17:51:39 +0200

    optimisation des routes

--- routes/beersController.js
+++ routes/beersController.js
@@ -3,74 +3,49 @@ const router = express.Router();
 const ObjectID = require('mongoose').Types.ObjectId;
 const { BeersModel } = require('../models/beersModel');
 
+// Read
+
 router.get('/', (req, res) => {
-    BeersModel.find((err, docs) => {
-        if (!err) {
-            res.send(docs)
-        } else {
-            console.log("Erreur de la requête: " + err);
-        };
-    })
+    BeersModel.find()
+        .then(BeersModel => res.status(200).json(BeersModel))
+        .catch(error => res.status(400).json({ error }));
 });
+
+// 404
+
+router.get('/:id', (req, res) => {
+    BeersModel.findOne({ _id: req.params.id })
+        .then(BeersModel => res.status(200).json(BeersModel))
+        .catch(error => res.status(404).json({ error }));
+});
+
+// Write
 
 router.post('/', (req, res) => {
+    delete req.body._id;
     const newProductBeer = new BeersModel({
-        name: req.body.name,
-        typeBeer: req.body.typeBeer
+        ...req.body
     });
 
-    newProductBeer.save((err, docs) => {
-        if (!err) {
-            res.send(docs)
-        } else {
-            console.log('Erreur de création : ' + err);
-        }
-    })
+    newProductBeer.save()
+        .then(() => res.status(201).json({ message: 'Produit enregistré'}))
+        .catch(error => res.status(400).json({ error }));
 });
 
-// update
+// Update
 
 router.put('/:id', (req, res) => {
-    if (!ObjectID.isValid(req.params.id)) {
-        return res.status(400).send('Erreur ID : ' + req.params.id)
-    }
-
-    const updateBeer = {
-        name: req.body.name,
-        type: req.body.type
-    };
-
-    BeersModel.findByIdAndUpdate(
-        req.params.id,
-        { $set: updateBeer },
-        { new: true },
-        (err, docs) => {
-            if (!err) {
-                res.send(docs);
-            } else {
-                console.log("Erreur update : " + err);
-            }
-        }
-    );
+    BeersModel.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Produit modifié'}))
+        .catch(error => res.status(400).json({ error }));
 });
 
+// Delete
+
 router.delete('/:id', (req, res) => {
-    if (!ObjectID.isValid(req.params.id)) {
-        return res.status(400).send('Erreur ID : ' + req.params.id)
-        };
-    
-
-
-    BeersModel.findByIdAndRemove(
-        req.params.id,
-        (err, docs) => {
-            if (!err) {
-                res.send(docs);
-            } else {
-                console.log("Erreur de suppression : " + err);
-            }
-        }
-    )
+    BeersModel.deleteOne({ _id: req.params.id})
+        .then(() => res.status(200).json({ message: 'Produit supprimé'}))
+        .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = router;
